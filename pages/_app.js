3a4a8b2d51f66bc627f9ea27c7f689d6874976a3
mellaniesoriano <23709723+mellaniesoriano@users.noreply.github.com>
2019-07-07 02:07:45 -1000

    🎉 Init
    First commit

---
+++ pages/_app.js
@@ -0,0 +1,29 @@
+import App, { Container } from "next/app";
+import React from "react";
+import GlobalStyle from "../styles/GlobalStyle";
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </>
+      </Container>
+    );
+  }
+}
+
+export default MyApp;
---
+++ styles/GlobalStyle.js
@@ -0,0 +1,61 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  ul, ol {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+  }
+  img {
+    width: 100%;
+    max-width: 100%;
+    vertical-align: middle;
+  }
+  svg {
+    fill: currentColor;
+  }
+  button {
+    cursor: pointer;
+    border: 0;
+    border-radius: 0;
+    &:focus,
+    &:active {
+      outline: 0;
+    }
+  }
+  input {
+    border-radius: 0;
+    outline: 0;
+    &:focus {
+      outline: 0;
+    }
+    &::placeholder {
+    }
+    &:focus,
+    &:active {
+      &::placeholder {
+        opacity: 0.5;
+      }
+    }
+  }
+`;
+
+export default GlobalStyle;
